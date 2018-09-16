fcb24db3367d62e23b0f98267e136a69985e4457
c7x43t <c7x43t@gmail.com>
2018-09-16 19:18:43 +0200

    Update toFastProperties.js

--- toFastProperties.js
+++ toFastProperties.js
@@ -15,10 +15,4 @@
         return FastObject(this);
     };
 })(Object.prototype);
-// Make Objects iterable [for of]
-Object.prototype[Symbol.iterator]=function* objectIterator(){
-	const keys=Object.keys(this);
-	for(let i=0;i<keys.length;i++){
-        yield keys[i];
-    }
-}
+
