9dda0088d12285af0c56e7165792d601feab090c
c7x43t <c7x43t@gmail.com>
2018-09-16 19:18:29 +0200

    Update toFastProperties.js

--- toFastProperties.js
+++ toFastProperties.js
@@ -1,13 +1,24 @@
 // https://github.com/sindresorhus/to-fast-properties/blob/master/index.js
-(function(d) {
-    function b(a) {
-        if (null !== c && typeof c.property) return a = c, c = b.prototype = null, a;
-        c = b.prototype = null == a ? Object.create(null) : a;
-        return new b
+(function toFastProperties(context) {
+    let fastProto = null;
+    function FastObject(o) {
+        if (fastProto !== null && typeof fastProto.property) {
+            const result = fastProto;
+            fastProto = FastObject.prototype = null;
+            return result;
+        }
+        fastProto = FastObject.prototype = o == null ? Object.create(null) : o;
+        return new FastObject;
     }
-    var c = null;
-    b();
-    d.toFastproperties = function(a) {
-        return b(a)
+    FastObject();
+    context.toFastproperties = function() {
+        return FastObject(this);
+    };
+})(Object.prototype);
+// Make Objects iterable [for of]
+Object.prototype[Symbol.iterator]=function* objectIterator(){
+	const keys=Object.keys(this);
+	for(let i=0;i<keys.length;i++){
+        yield keys[i];
     }
-})(window);
+}
